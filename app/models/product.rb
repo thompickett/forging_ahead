1312f2501026ee7c9e42512376e6cfdd8c78a32d
thomp <thompickett@gmail.com>
2016-11-18 17:57:53 -0700

    Add 'required: false' to products and productstlyes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ApplicationRecord
   has_many :product_styles
-  belongs_to :attachment
+  belongs_to :attachment, required: false
 end
--- app/models/product_styles.rb
+++ app/models/product_styles.rb
@@ -1,6 +1,6 @@
 class ProductStyles < ApplicationRecord
   belongs_to :product
   belongs_to :style
-  belongs_to :attachment
+  belongs_to :attachment, required: false
   has_many :attachments
 end
